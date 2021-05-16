cf64f89c605884bf014565c178661fca6a5685a7
Murat UGUR <69518995+mrugurs@users.noreply.github.com>
2021-05-16 04:40:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.setUserId(1);
-        student1.setUserName("mugur");
-        student1.setFirstName("Murat");
-        student1.setLastName("UGUR");
-        student1.setStudentAge(25);
-        student1.setEmail("murat@gmailcom");
-        student1.setPassword("dd64ed4");
 
-        Instructor instructor1 = new Instructor();
-        instructor1.setUserId(1);
-        instructor1.setUserName("mrugur");
-        instructor1.setFirstName("Mehmet Can");
-        instructor1.setLastName("UĞUR");
-        instructor1.setEmail("mrt@gmailcom");
-        instructor1.setPassword("cs56468se98wef");
-        instructor1.setGraduate("Ege Üniversitesi");
+        System.out.print("1. Öğrenci Kayıt\n2. Eğitmen Kayıt\n3. Bilgi Güncelleme\n4. Kullanıcı silme\nLütfen Bir Seçim Yapın: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
 
-
-        UserManager userManager4 = new StudentManager();
-        userManager4.Add(student1);
-
-        UserManager userManager3 = new InstructorManager();
-        userManager3.Add(instructor1);
-
+        if (input == 1) {
+            StudentManager userManager = new StudentManager();
+            userManager.Add();
+        }
+        else if (input == 2) {
+            UserManager userManager = new InstructorManager();
+            userManager.Add();
+        }
+        else if (input == 3){
+            UserManager userManager = new UserManager();
+            userManager.Update();
+        }
+        else if (input == 4){
+            UserManager userManager = new UserManager();
+            userManager.Remove();
+        }
+        else {
+            System.out.println("Geçersiz Giriş!!");
+        }
     }
 }
